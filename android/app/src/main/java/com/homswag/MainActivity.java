25689e7b5b25974fd92de3062b05ca6302082451
trdp30 <trdp30@gmail.com>
2020-03-15 14:43:39 +0530

    react-navigation build succesfull

--- android/app/src/main/java/com/homswag/MainActivity.java
+++ android/app/src/main/java/com/homswag/MainActivity.java
@@ -1,6 +1,11 @@
 package com.homswag;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+/*import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;*/
+import com.swmansion.gesturehandler.react.*;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +16,14 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "homswag";
+  }
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 }
