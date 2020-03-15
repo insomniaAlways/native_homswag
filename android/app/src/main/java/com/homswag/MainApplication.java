b6d5006aae7e1a03849f4650a2d7af08dd3ce01c
trdp30 <trdp30@gmail.com>
2020-03-15 17:22:31 +0530

    android build successful for ui-kitten library import and react-native svg

--- android/app/src/main/java/com/homswag/MainApplication.java
+++ android/app/src/main/java/com/homswag/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+// import com.horcrux.svg.SvgPackage;
 // import com.oblador.vectoricons.*;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           // packages.add(new VectorIconsPackage())
+          // packages.add(new new SvgPackage()())
           return packages;
         }
 
